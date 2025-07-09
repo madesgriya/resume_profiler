d11a0567b8445572fce3921d5bfd70aded72ab05
Matty <madesgputra@gmail.com>
2025-07-09 16:05:59 +0700

    :zap: Backups

---
+++ main.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+import sys
+import warnings
+
+from datetime import datetime
+
+from resume_profiler.crew import ResumeProfiler
+
+warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+
+# This main file is intended to be a way for you to run your
+# crew locally, so refrain from adding unnecessary logic into this file.
+# Replace with inputs you want to test with, it will automatically
+# interpolate any tasks and agents information
+
+def run():
+    """
+    Run the crew.
+    """
+    global inputs
+    inputs = {
+        'job_url': 'https://www.linkedin.com/jobs/view/4247758294',
+        'current_year': str(datetime.now().year)
+    }
+    try:
+        ResumeProfiler().crew().kickoff(inputs=inputs)
+    except Exception as e:
+        raise Exception(f"An error occurred while running the crew: {e}")
+
+
+def train():
+    """
+    Train the crew for a given number of iterations.
+    """
+    try:
+        ResumeProfiler().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while training the crew: {e}")
+
+def replay():
+    """
+    Replay the crew execution from a specific task.
+    """
+    try:
+        ResumeProfiler().crew().replay(task_id=sys.argv[1])
+
+    except Exception as e:
+        raise Exception(f"An error occurred while replaying the crew: {e}")
+
+def test():
+    """
+    Test the crew execution and returns the results.
+    """
+    try:
+        ResumeProfiler().crew().test(n_iterations=int(sys.argv[1]), eval_llm=sys.argv[2], inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while testing the crew: {e}")
